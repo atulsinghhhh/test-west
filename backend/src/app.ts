97c78d807bf6bd385c87b9966e5860566ab2675f
atulsinghhhh <atulsinghhh25@gmail.com>
2025-12-21 16:34:59 +0530

    add frotend url

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -12,7 +12,12 @@ import StandaloneStudentRoutes from "./routes/standaloneStudent.route.js"
 const app = express();
 
 // middleware 
-app.use(cors());
+app.use(cors(
+    {
+        origin: process.env.Frontend_URL || 'http://localhost:5173',
+        credentials: true,
+    }
+));
 app.use(express.json());
 app.use(cookieParser());
 
